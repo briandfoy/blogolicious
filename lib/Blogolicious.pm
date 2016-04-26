53a4dcc8218a71bc04ecb2f741c6f4b3171c9c27
brian d foy <brian.d.foy@gmail.com>
2016-04-25 21:55:26 -0400

    Initial module creation

---
+++ lib/Blogolicious.pm
@@ -0,0 +1,69 @@
+package Blogolicious;
+use strict;
+
+use warnings;
+no warnings;
+
+use subs qw();
+use vars qw($VERSION);
+
+$VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+Blogolicious - Figure out stuff about a blog
+
+=head1 SYNOPSIS
+
+	use Blogolicious;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/blogolicious/
+
+=head1 AUTHOR
+
+brian d foy, C<< <brian.d.foy@gmail.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+
+Copyright (c) 2016, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
